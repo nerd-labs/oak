353fe7f185c78fd0956c13459a0c1739f2e42fe9
Robbert Wolfs <robbertwolfs@gmail.com>
2018-09-04 20:31:44 +0200

    feat(PWA): add service worker, show offline page when offline

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -8,3 +10,16 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+if (navigator.serviceWorker.controller) {
+	console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+	//Register the ServiceWorker
+	navigator.serviceWorker
+		.register('sw.js', {
+    	scope: './'
+  	})
+		.then(reg => {
+			console.log('Service worker has been registered');
+		});
+}
